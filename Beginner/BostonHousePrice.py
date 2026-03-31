6cfb377a9785200490fe85bb1b7ccc14a139a32d
Shyam Sunder <sshyamlebtech24@ced.alliance.edu.in>
2026-03-31 23:32:50 +0530

    Import TensorFlow in BostonHousePrice.py
    Added TensorFlow import for potential model enhancements.

--- Beginner/BostonHousePrice.py
+++ Beginner/BostonHousePrice.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import tensorflow as tf
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
